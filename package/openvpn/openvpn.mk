6bde0fb1a36fff1a1c7e7aaeebb44b3d15fa2465
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    openvpn: update makefile
    Change-Id: I1a896daebd95f5af2d4d3ffc3740ce5eaba23576

--- package/openvpn/openvpn.mk
+++ package/openvpn/openvpn.mk
@@ -4,20 +4,21 @@
 #
 ################################################################################
 
-OPENVPN_VERSION = 2.4.3
-OPENVPN_SOURCE = openvpn-$(OPENVPN_VERSION).tar.xz
 OPENVPN_SITE = http://swupdate.openvpn.net/community/releases
+OPENVPN_VERSION_FILE = version.m4
 OPENVPN_DEPENDENCIES = host-pkgconf openssl
+OPENVPN_AUTORECONF = YES
 OPENVPN_LICENSE = GPL-2.0
 OPENVPN_LICENSE_FILES = COPYRIGHT.GPL
 OPENVPN_CONF_OPTS = \
 	--disable-plugin-auth-pam \
+	--disable-pam-dlopen \
 	--enable-iproute2 \
 	--with-crypto-library=openssl \
 	$(if $(BR2_STATIC_LIBS),--disable-plugins)
-OPENVPN_CONF_ENV = IFCONFIG=/sbin/ifconfig \
-	NETSTAT=/bin/netstat \
-	ROUTE=/sbin/route
+OPENVPN_CONF_ENV = \
+	CFLAGS="-I$(BR2_TOPDIR)$(ACOS)/include" \
+	LDFLAGS="-L$(TARGET_DIR)/usr/lib -lnvram"
 
 ifeq ($(BR2_PACKAGE_OPENVPN_SMALL),y)
 OPENVPN_CONF_OPTS += \
@@ -50,11 +51,6 @@ endif
 define OPENVPN_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 $(@D)/src/openvpn/openvpn \
 		$(TARGET_DIR)/usr/sbin/openvpn
-endef
-
-define OPENVPN_INSTALL_INIT_SYSV
-	$(INSTALL) -m 755 -D package/openvpn/S60openvpn \
-		$(TARGET_DIR)/etc/init.d/S60openvpn
 endef
 
 $(eval $(autotools-package))
