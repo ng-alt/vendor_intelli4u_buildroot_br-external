1c310acc4040582e3d8882ef29bc49f95f37bbda
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    radvd: update makefile
    Change-Id: I2543957c96d91753fb434da18cb7cc291333c135

--- package/radvd/radvd.mk
+++ package/radvd/radvd.mk
@@ -4,8 +4,6 @@
 #
 ################################################################################
 
-RADVD_VERSION = 2.12
-RADVD_SOURCE = radvd-$(RADVD_VERSION).tar.xz
 RADVD_SITE = http://www.litech.org/radvd/dist
 RADVD_DEPENDENCIES = host-bison flex host-flex host-pkgconf
 # We need to ignore <linux/if_arp.h>, because radvd already includes
@@ -17,9 +15,5 @@ RADVD_CONF_ENV = \
 RADVD_AUTORECONF = YES
 RADVD_LICENSE = BSD-4-Clause-like
 RADVD_LICENSE_FILES = COPYRIGHT
-
-define RADVD_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 package/radvd/S50radvd $(TARGET_DIR)/etc/init.d/S50radvd
-endef
 
 $(eval $(autotools-package))
