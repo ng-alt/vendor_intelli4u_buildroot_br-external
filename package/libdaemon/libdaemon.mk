960af07eefdc3f2ea6a1ff28a5b0ba3b014d4221
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    libdaemon: update makefile
    Change-Id: Iecc725471ecb0693098628c828a3297f85f98a97

--- package/libdaemon/libdaemon.mk
+++ package/libdaemon/libdaemon.mk
@@ -4,7 +4,6 @@
 #
 ################################################################################
 
-LIBDAEMON_VERSION = 0.14
 LIBDAEMON_SITE = http://0pointer.de/lennart/projects/libdaemon
 LIBDAEMON_LICENSE = LGPL-2.1+
 LIBDAEMON_LICENSE_FILES = LICENSE
@@ -12,5 +11,7 @@ LIBDAEMON_INSTALL_STAGING = YES
 LIBDAEMON_CONF_ENV = ac_cv_func_setpgrp_void=no
 LIBDAEMON_CONF_OPTS = --disable-lynx
 LIBDAEMON_DEPENDENCIES = host-pkgconf
+LIBDAEMON_AUTOGEN = YES
+LIBDAEMON_AUTOGEN_SCRIPT = bootstrap.sh
 
 $(eval $(autotools-package))
