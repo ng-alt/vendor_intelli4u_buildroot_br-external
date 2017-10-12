23ad5e601a29605cf3218c5b3d8e596a3c96221f
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    ipset: update makefile
    Change-Id: I558cff6f6b09b84a0830052638650c4df5f6f2ab

--- package/ipset/ipset.mk
+++ package/ipset/ipset.mk
@@ -4,13 +4,19 @@
 #
 ################################################################################
 
-IPSET_VERSION = 6.34
-IPSET_SOURCE = ipset-$(IPSET_VERSION).tar.bz2
+IPSET_VERSION_FILE = Makefile
+IPSET_VERSION_PATTERN = "^IPSET_VERSION:=(.+)"
 IPSET_SITE = http://ipset.netfilter.org
 IPSET_DEPENDENCIES = libmnl host-pkgconf
 IPSET_CONF_OPTS = --with-kmod=no
 IPSET_LICENSE = GPL-2.0
 IPSET_LICENSE_FILES = COPYING
-IPSET_INSTALL_STAGING = YES
+IPSET_MAKE_OPTS = binaries
 
-$(eval $(autotools-package))
+define IPSET_INSTALL_TARGET_CMDS
+	install -D $(@D)/ipset $(TARGET_DIR)/usr/sbin/ipset
+#	install -d $(TARGET_DIR)/usr/lib/ipset
+#	install $(@D)/*.so $(TARGET_DIR)/usr/lib/ipset
+endef
+
+$(eval $(make-package))
