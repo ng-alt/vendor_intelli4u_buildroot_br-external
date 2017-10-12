6e801c52f9998ee28847e7e6f0a7e90aab554e0a
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    gdbm: update makefile
    $(MAKE1) is used to fix the dependent issue to generate
    gram.h before referred.
    
     gdbmtool.c:20:18: fatal error: gram.h: No such file or directory
    
    Change-Id: Ia0eedc5d7ad0240e0d05ca10840b47d43c2029c2

--- package/gdbm/gdbm.mk
+++ package/gdbm/gdbm.mk
@@ -4,12 +4,14 @@
 #
 ################################################################################
 
-GDBM_VERSION = 1.13
-GDBM_SITE = $(BR2_GNU_MIRROR)/gdbm
+GDBM_VERSION_FILE = configure.ac
+GDBM_VERSION_PATTERN = "@(_GDBM_VERSION_MAJOR).@(_GDBM_VERSION_MINOR)"
 GDBM_LICENSE = GPL-3.0+
 GDBM_LICENSE_FILES = COPYING
+GDBM_AUTOGEN = YES
 GDBM_INSTALL_STAGING = YES
 GDBM_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES)
+GDBM_MAKE = $(MAKE1)
 
 ifeq ($(BR2_PACKAGE_READLINE),y)
 GDBM_CONF_OPTS += --with-readline
@@ -17,5 +19,11 @@ GDBM_DEPENDENCIES += readline
 else
 GDBM_CONF_OPTS += --without-readline
 endif
+
+# Not to install/compile the cracked data files
+define GDBM_SUPPESS_COMPILATION
+    $(SED) 's, po ,,' $(@D)/Makefile.am
+endef
+GDBM_PRE_CONFIGURE_HOOKS += GDBM_SUPPESS_COMPILATION
 
 $(eval $(autotools-package))
