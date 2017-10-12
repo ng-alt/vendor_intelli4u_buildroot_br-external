2339de86e37359851cf75118d5c7df2bdeb97985
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    libvorbis: update makefile
    Change-Id: Icc59f232939e7d880cfddc134ba9406bcf66fc07

--- package/libvorbis/libvorbis.mk
+++ package/libvorbis/libvorbis.mk
@@ -4,13 +4,17 @@
 #
 ################################################################################
 
-LIBVORBIS_VERSION = 1.3.5
-LIBVORBIS_SOURCE = libvorbis-$(LIBVORBIS_VERSION).tar.xz
 LIBVORBIS_SITE = http://downloads.xiph.org/releases/vorbis
+LIBVORBIS_AUTOGEN = YES
 LIBVORBIS_INSTALL_STAGING = YES
 LIBVORBIS_CONF_OPTS = --disable-oggtest
 LIBVORBIS_DEPENDENCIES = host-pkgconf libogg
 LIBVORBIS_LICENSE = BSD-3-Clause
 LIBVORBIS_LICENSE_FILES = COPYING
+
+define LIBVORBIS_DISABLE_UNNEEDED
+	$(SED) 's/SUBDIRS = m4 include vq lib examples test doc/SUBDIRS = m4 include vq lib/' $(@D)/Makefile.am
+endef
+LIBVORBIS_PRE_CONFIGURE_HOOKS += LIBVORBIS_DISABLE_UNNEEDED
 
 $(eval $(autotools-package))
