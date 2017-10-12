172f8a97c7ae83401d91dff3167ce05f4e3d075c
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    iperf: update package
    Change-Id: If7c99ce458ff1a64194c768e2b215500bbd4bfc5

--- package/iperf/iperf.mk
+++ package/iperf/iperf.mk
@@ -4,14 +4,22 @@
 #
 ################################################################################
 
-IPERF_VERSION = 2.0.10
 IPERF_SITE = http://downloads.sourceforge.net/project/iperf2
 IPERF_LICENSE = MIT-like
 IPERF_LICENSE_FILES = COPYING
-# patching configure.ac
-IPERF_AUTORECONF = YES
+IPERF_AUTOGEN = YES
+
+IPERF_CONF_ENV = \
+	ac_cv_func_malloc_0_nonnull=yes
 
 IPERF_CONF_OPTS = \
-	--disable-web100
+	--disable-web100 \
+	--disable-pam-dlopen \
+	--disable-plugin-auth-pam
+
+define IPERF_DISABLE_INSTALLATION
+    $(SED) 's/SUBDIRS = compat doc include src man/SUBDIRS = compat include src/' \$(@D)/Makefile.am
+endef
+IPERF_PRE_CONFIGURE_HOOKS += IPERF_DISABLE_INSTALLATION
 
 $(eval $(autotools-package))
