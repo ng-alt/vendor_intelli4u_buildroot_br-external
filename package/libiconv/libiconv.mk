d61f9b12b55a244d597838163b7af91a2b501897
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    libiconv: update makefile
    Change-Id: I317e39f853cd17cf48da458dfeec7708a4259f4c

--- package/libiconv/libiconv.mk
+++ package/libiconv/libiconv.mk
@@ -4,19 +4,29 @@
 #
 ################################################################################
 
-LIBICONV_VERSION = 1.15
 LIBICONV_SITE = $(BR2_GNU_MIRROR)/libiconv
-LIBICONV_INSTALL_STAGING = YES
 LIBICONV_LICENSE = GPL-3.0+ (iconv program), LGPL-2.0+ (library)
 LIBICONV_LICENSE_FILES = COPYING COPYING.LIB
+LIBICONV_AUTOGEN = YES
+LIBICONV_AUTOGEN_ENV = GNULIB_TOOL=$(BR2_TOPDIR)/external/gnulib/gnulib-tool
+#LIBICONV_AUTOGEN_OPTS = --skip-gnulib
 
 # Don't build the preloadable library, as we don't need it (it's only
 # for LD_PRELOAD to replace glibc's iconv, but we never build libiconv
 # when glibc is used). And it causes problems for static only builds.
-define LIBICONV_DISABLE_PRELOAD
+define LIBICONV_WASH_OUT_PACKAGE
+	rm -f $(@D)/gnulib-local/lib/*.diff
 	$(SED) '/preload/d' $(@D)/Makefile.in
+	$(SED) '/ po /d' $(@D)/Makefile.in
+	$(SED) 's/\-2.68//g' $(@D)/Makefile.devel $(@D)/libcharset/Makefile.devel $(@D)/preload/Makefile.devel
 endef
-LIBICONV_PRE_CONFIGURE_HOOKS += LIBICONV_DISABLE_PRELOAD
+LIBICONV_PRE_CONFIGURE_HOOKS += LIBICONV_WASH_OUT_PACKAGE
+
+define LIBICONV_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/lib/.libs/libiconv.so.2.5.1 $(TARGET_DIR)/usr/lib/libiconv.so.2.5.1
+	ln -sf libiconv.so.2.5.1 $(TARGET_DIR)/usr/lib/libiconv.so
+	ln -sf libiconv.so.2.5.1 $(TARGET_DIR)/usr/lib/libiconv.so.2
+endef
 
 $(eval $(autotools-package))
 
