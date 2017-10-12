8597ad0d1c411f82fad6d34f0b41c26e7ddaffeb
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    usb_modeswitch: update makefile
    Change-Id: Ic510062b93f818f8e234adb31355d6569de69886

--- package/usb_modeswitch/usb_modeswitch.mk
+++ package/usb_modeswitch/usb_modeswitch.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-USB_MODESWITCH_VERSION = 2.5.0
-USB_MODESWITCH_SOURCE = usb-modeswitch-$(USB_MODESWITCH_VERSION).tar.bz2
-USB_MODESWITCH_SITE = http://www.draisberghof.de/usb_modeswitch
+USB_MODESWITCH_VERSION_FILE = $(BUILDROOT)external/usb_modeswitch/Makefile
+USB_MODESWITCH_VERSION_PATTERN = "VERS\s*=\s*([^ ]+)"
 USB_MODESWITCH_DEPENDENCIES = libusb
 USB_MODESWITCH_LICENSE = GPL-2.0+
 USB_MODESWITCH_LICENSE_FILES = COPYING
@@ -29,14 +28,21 @@ USB_MODESWITCH_POST_PATCH_HOOKS += UPDATE_CONFIG_HOOK
 define USB_MODESWITCH_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
 		CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE -Wall -I." \
+		LDFLAGS="-ldl" \
 		JIM_CONFIGURE_OPTS="--host=$(GNU_TARGET_NAME) --build=$(GNU_HOST_NAME)" \
 		-C $(@D) $(USB_MODESWITCH_BUILD_TARGETS)
 endef
 
 define USB_MODESWITCH_INSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE -Wall -I." \
+		LDFLAGS="-ldl" \
 		DESTDIR=$(TARGET_DIR) \
 		-C $(@D) $(USB_MODESWITCH_INSTALL_TARGETS)
+	if [ -d $(TARGET_DIR)/rom/etc ] ; then \
+		$(INSTALL) -D -m 0644 $(@D)/usb_modeswitch.conf $(TARGET_DIR)/rom/etc/usb_modeswitch.conf; \
+		$(SED) 's/#.*//g;s/[ \t]\+/ /g;s/^[ \t]*//;s/[ \t]*$$//;/^$$/d' $(TARGET_DIR)/rom/etc/usb_modeswitch.conf; \
+	fi
 endef
 
 $(eval $(generic-package))
