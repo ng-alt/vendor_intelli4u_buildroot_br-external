c78984fe6d61f4376ffd38647b2e94c954464695
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    netstat-nat: update package
    It updates makefile to override default install command
    
    Change-Id: I9857f3a9f00ea5f154338a100812578fa23b5c45

--- package/netstat-nat/netstat-nat.mk
+++ package/netstat-nat/netstat-nat.mk
@@ -4,9 +4,12 @@
 #
 ################################################################################
 
-NETSTAT_NAT_VERSION = 1.4.10
 NETSTAT_NAT_SITE = http://tweegy.nl/download
 NETSTAT_NAT_LICENSE = GPL-2.0+
 NETSTAT_NAT_LICENSE_FILES = COPYING
+
+define NETSTAT_NAT_INSTALL_TARGET_CMDS
+	$(INSTALL) $(@D)/netstat-nat $(TARGET_DIR)/usr/sbin/netstat-nat
+endef
 
 $(eval $(autotools-package))
