79e6e224494ab47374f788d08f425903b9d12f62
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    vsftpd: update makefile
    Change-Id: I93a50300e2261b6d5ac8b2fa8e6e4096137542a0

--- package/vsftpd/vsftpd.mk
+++ package/vsftpd/vsftpd.mk
@@ -4,28 +4,34 @@
 #
 ################################################################################
 
-VSFTPD_VERSION = 3.0.3
-VSFTPD_SITE = https://security.appspot.com/downloads
 VSFTPD_LIBS = -lcrypt
 VSFTPD_LICENSE = GPL-2.0
 VSFTPD_LICENSE_FILES = COPYING
+VSFTPD_DEPENDENCIES = router
 
 define VSFTPD_DISABLE_UTMPX
 	$(SED) 's/.*VSF_BUILD_UTMPX/#undef VSF_BUILD_UTMPX/' $(@D)/builddefs.h
 endef
 
 define VSFTPD_ENABLE_SSL
+	cp $(@D)/builddefs-ssl.h $(@D)/builddefs.h
 	$(SED) 's/.*VSF_BUILD_SSL/#define VSF_BUILD_SSL/' $(@D)/builddefs.h
 endef
 
-ifeq ($(BR2_PACKAGE_VSFTPD_UTMPX),)
-VSFTPD_POST_CONFIGURE_HOOKS += VSFTPD_DISABLE_UTMPX
-endif
+define VSFTPD_DISABLE_SSL
+	cp $(@D)/builddefs-nossl.h $(@D)/builddefs.h
+endef
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 VSFTPD_DEPENDENCIES += openssl host-pkgconf
 VSFTPD_LIBS += `$(PKG_CONFIG_HOST_BINARY) --libs libssl libcrypto`
 VSFTPD_POST_CONFIGURE_HOOKS += VSFTPD_ENABLE_SSL
+else
+VSFTPD_POST_CONFIGURE_HOOKS += VSFTPD_DISABLE_SSL
+endif
+
+ifeq ($(BR2_PACKAGE_VSFTPD_UTMPX),)
+VSFTPD_POST_CONFIGURE_HOOKS += VSFTPD_DISABLE_UTMPX
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCAP),y)
@@ -38,17 +44,14 @@ VSFTPD_DEPENDENCIES += linux-pam
 VSFTPD_LIBS += -lpam
 endif
 
+VSFTPD_PRE_BUILD_HOOKS += ROUTER_MAKEFILE_INCLUDE_SUPPRESS_SUBROUTINE
+
 define VSFTPD_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" \
-		LDFLAGS="$(TARGET_LDFLAGS)" LIBS="$(VSFTPD_LIBS)" -C $(@D)
-endef
-
-define VSFTPD_USERS
-	ftp -1 ftp -1 * /home/ftp - - Anonymous FTP User
-endef
-
-define VSFTPD_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 755 package/vsftpd/S70vsftpd $(TARGET_DIR)/etc/init.d/S70vsftpd
+	$(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" LIBS="$(VSFTPD_LIBS)" \
+		ROUTER_DIR="$(ROUTER_DIR)" OPENSSLDIR="$(OPENSSL_DIR)" SRCBASE="$(SRC_BASE_DIR)" \
+		TOP="$(if $(ROOTDIR),$(ROUTER_DIR),$(TOP))" \
+		LINUXDIR=$(if $(LINUX_OVERRIDE2_SRCDIR),$(LINUX_OVERRIDE2_SRCDIR),$(LINUX_DIR)) \
+		-C $(@D) vsftpd
 endef
 
 # vsftpd won't work if the jail directory is writable, it has to be
@@ -60,8 +63,6 @@ define VSFTPD_INSTALL_TARGET_CMDS
 	test -f $(TARGET_DIR)/etc/vsftpd.conf || \
 		$(INSTALL) -D -m 644 $(@D)/vsftpd.conf \
 			$(TARGET_DIR)/etc/vsftpd.conf
-	$(INSTALL) -d -m 700 $(TARGET_DIR)/usr/share/empty
-	$(INSTALL) -d -m 555 $(TARGET_DIR)/home/ftp
 endef
 
 $(eval $(generic-package))
