6250b33fb0d56c33fa6ca10845140bd381b36c56
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    libubox: update makefile
    Change-Id: I7cd1ec68371cb23b46249c4016d5b8e1c578fa6c

--- package/libubox/libubox.mk
+++ package/libubox/libubox.mk
@@ -4,19 +4,15 @@
 #
 ################################################################################
 
-LIBUBOX_VERSION = 723730214732862de7e892bae2f7a7889e66bf1b
-LIBUBOX_SITE = git://git.openwrt.org/project/libubox.git
-LIBUBOX_LICENSE = ISC, BSD-3-Clause
+LIBUBOX_LICENSE =  BSD 3-Clause
 LIBUBOX_INSTALL_STAGING = YES
-LIBUBOX_DEPENDENCIES = $(if $(BR2_PACKAGE_JSON_C),json-c)
 
-ifeq ($(BR2_USE_MMU)$(BR2_PACKAGE_LUA_5_1),yy)
-LIBUBOX_DEPENDENCIES += lua
-LIBUBOX_CONF_OPTS += -DBUILD_LUA=ON \
-	-DLUAPATH=/usr/lib/lua/5.1 \
-	-DLUA_CFLAGS=-I$(STAGING_DIR)/usr/include
-else
-LIBUBOX_CONF_OPTS += -DBUILD_LUA=OFF
-endif
+define LIBUBOX_INSTALL_STAGING_CMDS
+	$(INSTALL) $(@D)/libubox.a $(STAGING_DIR)/usr/lib/libubox.a
+endef
 
-$(eval $(cmake-package))
+define LIBUBOX_INSTALL_TARGET_CMDS
+	$(INSTALL) $(@D)/libubox.a $(TARGET_DIR)/usr/lib/libubox.a
+endef
+
+$(eval $(make-package))
