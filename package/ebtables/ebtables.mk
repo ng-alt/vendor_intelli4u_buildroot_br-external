c9503e09e7fb552100097dc0ae1ae756f473389d
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    ebtables: update makefile
    Change-Id: I2bda3c86a9e91a692a5c90ba3445d103bc7aaa34

--- package/ebtables/ebtables.mk
+++ package/ebtables/ebtables.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-EBTABLES_VERSION = 2.0.10-4
-EBTABLES_SOURCE = ebtables-v$(EBTABLES_VERSION).tar.gz
+EBTABLES_VERSION_FILE = Makefile
+EBTABLES_VERSION_PATTERN = "^PROGVERSION_:=(.+)"
 EBTABLES_SITE = http://ftp.netfilter.org/pub/ebtables
 EBTABLES_LICENSE = GPL-2.0+
 EBTABLES_LICENSE_FILES = COPYING
@@ -13,7 +13,7 @@ EBTABLES_STATIC = $(if $(BR2_STATIC_LIBS),static)
 EBTABLES_K64U32 = $(if $(BR2_KERNEL_64_USERLAND_32),-DKERNEL_64_USERSPACE_32)
 
 define EBTABLES_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) LIBDIR=/lib/ebtables $(EBTABLES_STATIC) \
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) LIBDIR=/usr/lib/ebtables $(EBTABLES_STATIC) \
 		CFLAGS="$(TARGET_CFLAGS) $(EBTABLES_K64U32)" -C $(@D)
 endef
 
@@ -28,7 +28,7 @@ define EBTABLES_INSTALL_TARGET_CMDS
 		$(@D)/$(EBTABLES_SUBDIR)/extensions/*.so; \
 		do \
 		$(INSTALL) -m 0755 -D $${so} \
-			$(TARGET_DIR)/lib/ebtables/`basename $${so}` || exit 1; \
+			$(TARGET_DIR)/usr/lib/ebtables/`basename $${so}` || exit 1; \
 	done
 	$(INSTALL) -m 0755 -D $(@D)/$(EBTABLES_SUBDIR)/ebtables \
 		$(TARGET_DIR)/sbin/ebtables
