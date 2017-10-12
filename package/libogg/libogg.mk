bdf08965a73df7d3a02523c33baead97b14b9aec
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    libogg: update makefile
    Change-Id: I6ba7fd73c0de097a179544fcd17f41bd0185ac47

--- package/libogg/libogg.mk
+++ package/libogg/libogg.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-LIBOGG_VERSION = 1.3.2
-LIBOGG_SOURCE = libogg-$(LIBOGG_VERSION).tar.xz
 LIBOGG_SITE = http://downloads.xiph.org/releases/ogg
 LIBOGG_LICENSE = BSD-3-Clause
 LIBOGG_LICENSE_FILES = COPYING
-
+LIBOGG_AUTOGEN = YES
 LIBOGG_INSTALL_STAGING = YES
 LIBOGG_DEPENDENCIES = host-pkgconf
 
