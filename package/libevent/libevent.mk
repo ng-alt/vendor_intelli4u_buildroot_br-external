b31bdc1add6b1430106777e9582ae62f25523d3e
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    libevent: update makefile
    Change-Id: Ie4f93352d745b3d6177d71e7211419e5dc9909b7

--- package/libevent/libevent.mk
+++ package/libevent/libevent.mk
@@ -4,15 +4,13 @@
 #
 ################################################################################
 
-LIBEVENT_VERSION = 2.1.8-stable
-LIBEVENT_SITE = https://github.com/libevent/libevent/releases/download/release-$(LIBEVENT_VERSION)
-LIBEVENT_INSTALL_STAGING = YES
+LIBEVENT_SITE = https://github.com/libevent/libevent/releases/download
 LIBEVENT_LICENSE = BSD-3-Clause, OpenBSD
 LIBEVENT_LICENSE_FILES = LICENSE
-# For 0001-Disable-building-test-programs.patch
+LIBEVENT_INSTALL_STAGING = YES
+LIBEVENT_MAKE = $(MAKE1)
 LIBEVENT_AUTORECONF = YES
 LIBEVENT_CONF_OPTS = --disable-samples
-HOST_LIBEVENT_CONF_OPTS = --disable-samples --disable-openssl
 
 define LIBEVENT_REMOVE_PYSCRIPT
 	rm $(TARGET_DIR)/usr/bin/event_rpcgen.py
@@ -24,12 +22,11 @@ ifneq ($(BR2_PACKAGE_PYTHON),y)
 LIBEVENT_POST_INSTALL_TARGET_HOOKS += LIBEVENT_REMOVE_PYSCRIPT
 endif
 
-ifeq ($(BR2_PACKAGE_LIBOPENSSL),y)
-LIBEVENT_DEPENDENCIES += host-pkgconf libopenssl
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+LIBEVENT_DEPENDENCIES += host-pkgconf openssl
 LIBEVENT_CONF_OPTS += --enable-openssl
 else
 LIBEVENT_CONF_OPTS += --disable-openssl
 endif
 
 $(eval $(autotools-package))
-$(eval $(host-autotools-package))
