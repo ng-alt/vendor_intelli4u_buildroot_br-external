9f5a33e17e48d2f36675088300bc35df2c062837
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    minidlna: update makefile
    Change-Id: Iaa5ce6113fb5652c240118b4515891df5aeb368d

--- package/minidlna/minidlna.mk
+++ package/minidlna/minidlna.mk
@@ -4,15 +4,22 @@
 #
 ################################################################################
 
-MINIDLNA_VERSION = 1.2.0
-MINIDLNA_SITE = http://downloads.sourceforge.net/project/minidlna/minidlna/$(MINIDLNA_VERSION)
+MINIDLNA_SITE = http://downloads.sourceforge.net/project/minidlna/minidlna
+MINIDLNA_VERSION_FILE = upnpglobalvars.h
 MINIDLNA_LICENSE = GPL-2.0, BSD-3-Clause
 MINIDLNA_LICENSE_FILES = COPYING LICENCE.miniupnpd
 
 MINIDLNA_DEPENDENCIES = \
 	$(TARGET_NLS_DEPENDENCIES) \
-	ffmpeg flac libvorbis libogg libid3tag libexif jpeg sqlite \
-	host-xutil_makedepend
+	flac libvorbis libogg libid3tag libexif libjpeg sqlite
+
+ifeq ($(BR2_PACKAGE_FFMPEG),y)
+MINIDLNA_DEPENDENCIES += ffmpeg
+MINIDLNA_AVLIB_DIR = $(FFMPEG_DIR)
+else ifeq ($(BR2_PACKAGE_LIBAV),y)
+MINIDLNA_DEPENDENCIES += libav
+MINIDLNA_AVLIB_DIR = $(LIBAV_DIR)
+endif
 
 MINIDLNA_CONF_OPTS = \
 	--disable-static
@@ -23,19 +30,21 @@ endef
 
 MINIDLNA_POST_INSTALL_TARGET_HOOKS += MINIDLNA_INSTALL_CONF
 
-define MINIDLNA_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 package/minidlna/S60minidlnad \
-		$(TARGET_DIR)/etc/init.d/S60minidlnad
+define MINIDLNA_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) \
+		CFLAGS="$(ZIP_TARGET_CFLAGS) $(ZIP_CFLAGS) -I$(MINIDLNA_AVLIB_DIR) -I$(MINIDLNA_AVLIB_DIR)/libavcodec \
+		-I$(MINIDLNA_AVLIB_DIR)/libavformat -I$(MINIDLNA_AVLIB_DIR)/libavutil -I$(SQLITE_DIR) -I$(LIBEXIF_DIR) \
+		-I$(LIBID3TAG_DIR) -I$(LIBOGG_DIR) -I$(LIBVORBIS_DIR) -I$(LIBJPEG_DIR) -Iinclude" \
+		AS="$(TARGET_CC) -c" MINIDLNA_AVLIB_DIR=$(MINIDLNA_AVLIB_DIR) SQLITE_DIR=$(SQLITE_DIR) \
+		LIBEXIF_DIR=$(LIBEXIF_DIR) LIBID3TAG_DIR=$(LIBID3TAG_DIR) \
+		LIBOGG_DIR=$(LIBOGG_DIR) LIBVORBIS_DIR=$(LIBVORBIS_DIR) \
+		LIBJPEG_DIR=$(LIBJPEG_DIR)
 endef
 
-define MINIDLNA_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 0644 package/minidlna/minidlnad.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/minidlnad.service
-
-	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
-
-	ln -fs  ../../../../usr/lib/systemd/system/minidlnad.service \
-		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/minidlnad.service
+define MINIDLNA_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) install \
+		DESTDIR=$(TARGET_DIR) TARGETDIR=$(TARGET_DIR) PREFIX=$(TARGET_DIR)
+	ln -sf minidlna $(TARGET_DIR)/usr/sbin/minidlna.exe
 endef
 
-$(eval $(autotools-package))
+$(eval $(generic-package))
