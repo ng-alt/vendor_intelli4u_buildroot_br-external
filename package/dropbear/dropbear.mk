b718f92effab75434107f38a69a139e5db034e19
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    dropbear: update makefile
    It's to remove systemd and sysv rules.
    
    Change-Id: I607cdab5abe1ed4349b25461607140e0cde13997

--- package/dropbear/dropbear.mk
+++ package/dropbear/dropbear.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-DROPBEAR_VERSION = 2017.75
-DROPBEAR_SITE = http://matt.ucc.asn.au/dropbear/releases
-DROPBEAR_SOURCE = dropbear-$(DROPBEAR_VERSION).tar.bz2
+DROPBEAR_VERSION_FILE = sysoptions.h
+DROPBEAR_VERSION_PATTERN = "\#\s*define\s+DROPBEAR_VERSION\s+\"([^\"]+)\""
 DROPBEAR_LICENSE = MIT, BSD-2-Clause-like, BSD-2-Clause
 DROPBEAR_LICENSE_FILES = LICENSE
 DROPBEAR_TARGET_BINS = dropbearkey dropbearconvert scp
 DROPBEAR_PROGRAMS = dropbear $(DROPBEAR_TARGET_BINS)
+DROPBEAR_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_DROPBEAR_CLIENT),y)
 # Build dbclient, and create a convenience symlink named ssh
@@ -51,23 +51,6 @@ define DROPBEAR_DISABLE_STANDALONE
 	$(SED) 's:\(#define NON_INETD_MODE\):/*\1 */:' $(@D)/options.h
 endef
 
-define DROPBEAR_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 644 package/dropbear/dropbear.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/dropbear.service
-	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
-	ln -fs ../../../../usr/lib/systemd/system/dropbear.service \
-		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/dropbear.service
-endef
-
-ifeq ($(BR2_USE_MMU),y)
-define DROPBEAR_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 755 package/dropbear/S50dropbear \
-		$(TARGET_DIR)/etc/init.d/S50dropbear
-endef
-else
-DROPBEAR_POST_EXTRACT_HOOKS += DROPBEAR_DISABLE_STANDALONE
-endif
-
 ifeq ($(BR2_PACKAGE_DROPBEAR_DISABLE_REVERSEDNS),)
 DROPBEAR_POST_EXTRACT_HOOKS += DROPBEAR_ENABLE_REVERSE_DNS
 endif
@@ -89,11 +72,10 @@ DROPBEAR_CONF_OPTS += --disable-lastlog
 endif
 
 define DROPBEAR_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 $(@D)/dropbearmulti $(TARGET_DIR)/usr/sbin/dropbear
-	for f in $(DROPBEAR_TARGET_BINS); do \
-		ln -snf ../sbin/dropbear $(TARGET_DIR)/usr/bin/$$f ; \
+	$(INSTALL) -m 755 $(@D)/dropbearmulti $(TARGET_DIR)/usr/sbin/dropbearmulti
+	for f in $(DROPBEAR_PROGRAMS); do \
+		ln -snf ../sbin/dropbearmulti $(TARGET_DIR)/usr/bin/$$f ; \
 	done
-	ln -snf /var/run/dropbear $(TARGET_DIR)/etc/dropbear
 endef
 
 $(eval $(autotools-package))
