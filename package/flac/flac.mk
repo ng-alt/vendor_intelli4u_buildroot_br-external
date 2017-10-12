46c9e725a43aa9531a4fa530b44420805fa185cd
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    flac: update makefile
    Change-Id: Ib53da98cbbfa6defe1a8a9c905b8baa9a6124940

--- package/flac/flac.mk
+++ package/flac/flac.mk
@@ -4,15 +4,13 @@
 #
 ################################################################################
 
-FLAC_VERSION = 1.3.2
 FLAC_SITE = http://downloads.xiph.org/releases/flac
-FLAC_SOURCE = flac-$(FLAC_VERSION).tar.xz
 FLAC_INSTALL_STAGING = YES
 FLAC_DEPENDENCIES = $(if $(BR2_PACKAGE_LIBICONV),libiconv)
 FLAC_LICENSE = Xiph BSD-like (libFLAC), GPL-2.0+ (tools), LGPL-2.1+ (other libraries)
 FLAC_LICENSE_FILES = COPYING.Xiph COPYING.GPL COPYING.LGPL
 # 0001-configure.ac-relax-linux-OS-detection.patch patches configure.ac
-FLAC_AUTORECONF = YES
+FLAC_AUTOGEN = YES
 FLAC_CONF_OPTS = \
 	--disable-cpplibs \
 	--disable-xmms-plugin \
@@ -31,5 +29,15 @@ FLAC_CONF_OPTS += --enable-sse
 else
 FLAC_CONF_OPTS += --disable-sse
 endif
+
+define FLAC_COPY_MISSED
+	if [ ! -e $(@D)/config.rpath ] && [ -e /usr/share/gettext/config.rpath ] ; then cp /usr/share/gettext/config.rpath $(@D); fi
+endef
+
+define FLAC_DISABLE_UNNEEDED
+	$(SED) 's/SUBDIRS = doc include m4 man src examples test build obj/SUBDIRS = include m4 src build obj/' \
+		$(@D)/Makefile.am
+endef
+FLAC_PRE_CONFIGURE_HOOKS += FLAC_DISABLE_UNNEEDED FLAC_COPY_MISSED
 
 $(eval $(autotools-package))
