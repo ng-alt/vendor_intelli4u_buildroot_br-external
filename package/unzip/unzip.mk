66297dfc172e8c0f9adbb4a3d1515b88995bca0e
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    unzip: update makefile
    Change-Id: I57a2ca7fee02605abd93e4b5c3dd3080e70774db

--- package/unzip/unzip.mk
+++ package/unzip/unzip.mk
@@ -4,12 +4,15 @@
 #
 ################################################################################
 
-UNZIP_VERSION = 60
-UNZIP_SOURCE = unzip$(UNZIP_VERSION).tgz
+UNZIP_VERSION_FILE = unzvers.h
+UNZIP_VERSION_PATTERN = "^\#define\s+UZ_VER_STRING\s+\"([^\"]+)\""
 UNZIP_SITE = ftp://ftp.info-zip.org/pub/infozip/src
-# take precedence over busybox implementation
-UNZIP_DEPENDENCIES = $(if $(BR2_PACKAGE_BUSYBOX),busybox)
 UNZIP_LICENSE = Info-ZIP
 UNZIP_LICENSE_FILES = LICENSE
+# take precedence over busybox implementation
+UNZIP_DEPENDENCIES = $(if $(BR2_PACKAGE_BUSYBOX),busybox)
+UNZIP_INSTALL_TARGET_OPTS = prefix=$(TARGET_DIR)/usr/sbin install
 
-$(eval $(cmake-package))
+UNZIP_PRE_BUILD_HOOKS = ROUTER_MAKEFILE_INCLUDE_SUPPRESS_SUBROUTINE
+
+$(eval $(make-package))
