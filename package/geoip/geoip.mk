5ceb2295a95e0bf6f5c0266e7050b15296edc310
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    geoip: update makefile
    Change-Id: If03e0a66162cea5d1af3c5de76867428a793db87

--- package/geoip/geoip.mk
+++ package/geoip/geoip.mk
@@ -4,11 +4,17 @@
 #
 ################################################################################
 
-GEOIP_VERSION = 1.6.11
-GEOIP_SOURCE = GeoIP-$(GEOIP_VERSION).tar.gz
-GEOIP_SITE = https://github.com/maxmind/geoip-api-c/releases/download/v$(GEOIP_VERSION)
+GEOIP_SITE = https://github.com/maxmind/geoip-api-c/releases/download
+GEOIP_AUTOGEN = YES
+GEOIP_AUTOGEN_SCRIPT = bootstrap
 GEOIP_INSTALL_STAGING = YES
 GEOIP_LICENSE = LGPL-2.1+
 GEOIP_LICENSE_FILES = COPYING
+
+define GEOIP_ADD_MISSED_FILES
+	rsync -au $(BR2_EXTERNAL_NETGEAR_PATH)/package/geoip/GeoIP.dat \
+		    $(@D)/data/GeoIP.dat
+endef
+GEOIP_PRE_CONFIGURE_HOOKS += GEOIP_ADD_MISSED_FILES
 
 $(eval $(autotools-package))
