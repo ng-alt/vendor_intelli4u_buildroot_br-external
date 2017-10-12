c42a7171893eeacfd2ae50715156f6944f859d45
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    sdparm: update makefile
    Change-Id: Ic64ffef3908f9c017f0eea4d6ec52ba36e4a2f56

--- package/sdparm/sdparm.mk
+++ package/sdparm/sdparm.mk
@@ -4,11 +4,9 @@
 #
 ################################################################################
 
-SDPARM_VERSION = 1.10
-SDPARM_SOURCE = sdparm-$(SDPARM_VERSION).tar.xz
-SDPARM_SITE = http://sg.danny.cz/sg/p
 SDPARM_LICENSE = BSD-3-Clause
 SDPARM_LICENSE_FILES = COPYING
+SDPARM_AUTOGEN = YES
 
 ifeq ($(BR2_PACKAGE_SG3_UTILS),y)
 SDPARM_DEPENDENCIES += sg3_utils
