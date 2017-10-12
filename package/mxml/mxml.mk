6d37466e36c549951f3fae59c7cdb1f983f0a4c3
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    mxml: update package
    It's to update the commands to build with cross-compilation as
    the original makefile will use its own generated tool to generate
    docs and man pages.
    
    Change-Id: I0aaa672d2304db35ed7016184ee6986574b51daa

--- package/mxml/mxml.mk
+++ package/mxml/mxml.mk
@@ -4,13 +4,23 @@
 #
 ################################################################################
 
-MXML_VERSION = 2.10
-MXML_SITE = https://github.com/michaelrsweet/mxml/releases/download/release-$(MXML_VERSION)
+MXML_SITE = https://github.com/michaelrsweet/mxml/releases/download
 MXML_LICENSE = LGPL-2.0+ with exceptions
 MXML_LICENSE_FILES = COPYING
 MXML_INSTALL_STAGING = YES
+MXML_MAKE_OPTS = libmxml.a libmxml.so.1.5
 
-MXML_INSTALL_STAGING_OPTS = DSTROOT=$(STAGING_DIR) install
-MXML_INSTALL_TARGET_OPTS = DSTROOT=$(TARGET_DIR) install
+define MXML_INSTALL_STAGING_CMDS
+	$(INSTALL) -D $(@D)/libmxml.a $(STAGING_DIR)/usr/lib/libmxml.a
+	$(INSTALL) -D $(@D)/libmxml.so.1.5 $(STAGING_DIR)/usr/lib/libmxml.so.1.5
+	$(INSTALL) -D $(@D)/mxml.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D $(@D)/mxml.pc $(STAGING_DIR)/usr/lib/pkgconfig/mxml.pc
+endef
+
+define MXML_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/mxml.h $(TARGET_DIR)/usr/include/
+	$(INSTALL) -D $(@D)/libmxml.so.1.5 $(TARGET_DIR)/usr/lib/libmxml.so.1.5
+	$(INSTALL) -D $(@D)/mxml.pc $(TARGET_DIR)/usr/lib/pkgconfig/mxml.pc
+endef
 
 $(eval $(autotools-package))
