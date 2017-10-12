025c03d4a546264e777ab39d432cde0030a42bba
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    hotplug2: update makefile
    Change-Id: I35049dbcb16ba330d50e9596d3d92b007b9890cc

--- package/hotplug2/hotplug2.mk
+++ package/hotplug2/hotplug2.mk
@@ -7,9 +7,14 @@
 HOTPLUG2_LICENSE = GPLv2
 HOTPLUG2_LICENSE_FILES = COPYING
 
+HOTPLUG2_DEPENDENCIES = $(if $(BR2_PACKAGE_ROUTER),router)
+
 # install the prog directly not to use the confused one in Makefile
 define HOTPLUG2_INSTALL_TARGET_CMDS
 	$(INSTALL) $(@D)/hotplug2 $(TARGET_DIR)/sbin/
+	if [ -e $(TARGET_DIR)/rom ] ; then \
+		$(INSTALL) $(@D)/examples/hotplug2.rules-2.6kernel $(TARGET_DIR)/rom/etc/hotplug2.rules; \
+	fi
 endef
 
 $(eval $(make-package))
