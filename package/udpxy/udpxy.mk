7101e9a2b3983cbb8f38f3cad28cbaacb6099fa1
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    udpxy: update makefile with source code
    Change-Id: I7a34d33e12b02a2b1b9d09f0da56969c0802a5ae

--- package/udpxy/udpxy.mk
+++ package/udpxy/udpxy.mk
@@ -7,16 +7,17 @@
 UDPXY_VERSION = 1.0.23-9-prod
 UDPXY_SOURCE = udpxy.$(UDPXY_VERSION).tar.gz
 UDPXY_SITE = http://www.udpxy.com/download/1_23
+UDPXY_SUBDIR = chipmunk
 UDPXY_LICENSE = GPL-3.0+
 UDPXY_LICENSE_FILES = README
 
 define UDPXY_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) NO_UDPXREC=yes -C $(@D)/$(UDPXY_SUBDIR)
 endef
 
 define UDPXY_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR=$(TARGET_DIR) PREFIX=/usr \
-		-C $(@D) install
+	$(TARGET_MAKE_ENV) $(MAKE) NO_UDPXREC=yes DESTDIR=$(TARGET_DIR) PREFIX=/usr \
+		-C $(@D)/$(UDPXY_SUBDIR) install
 endef
 
 $(eval $(generic-package))
