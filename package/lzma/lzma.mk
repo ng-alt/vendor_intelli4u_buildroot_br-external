cc57d34e8e20f8a7308074962ae91fc24bb6d91e
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    lzma: update makefile
    Change-Id: I6d1ebb38eafd9645674e77bd637cd708cc129dd1

--- package/lzma/lzma.mk
+++ package/lzma/lzma.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-LZMA_VERSION = 4.32.7
-LZMA_SOURCE = lzma-$(LZMA_VERSION).tar.xz
+LZMA_LICENSE = LGPL-2.1 or GPL-3.0
+LZMA_LICENSE_FILES = COPYING.LGPLv2.1 COPYING.GPLv3
 LZMA_SITE = http://tukaani.org/lzma
+LZMA_AUTOGEN = YES
 
 $(eval $(host-autotools-package))
 
