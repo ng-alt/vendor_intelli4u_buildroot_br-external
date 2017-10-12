241e9a858cfe6f3e15cff21f7532fa80e4f7b051
ccc <cadappl@gmail.com>
2017-10-12 22:39:27 +0800

    udev: update makefile
    Change-Id: I88c5dc5cdb4f732c75548578f6ef21aeb0d31959

--- package/udev/udev.mk
+++ package/udev/udev.mk
@@ -11,7 +11,7 @@ UDEV_LICENSE_FILE = COPYGING
 UDEV_MAKE_ENV = CROSS_COMPILE="$(TARGET_CROSS)"
 UDEV_MAKE_OPTS = all
 
-UDEV_INSTALL_TARGET_OPTS = prefix=$(TARGET_DIR) install-udevtrigger
+UDEV_INSTALL_TARGET_OPTS = prefix=$(TARGET_DIR) install-bin
 
 define UDEV_FIX_LIBVOLUME_ID_SOFTLINK
 	$(SED) 's,ln -sf $$(libdir)/$$(SHLIB) $$(DESTDIR)$$(usrlibdir)/libvolume_id.so,ln -sf /lib/$$(SHLIB) $$(DESTDIR)$$(usrlibdir)/libvolume_id.so,' \
