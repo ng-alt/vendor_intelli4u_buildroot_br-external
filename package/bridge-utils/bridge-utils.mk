59d813b382376fc3977652e30ec41681750f0ce9
ccc <cadappl@gmail.com>
2018-10-19 23:15:24 +0800

    bridge-utils: update package
    Change-Id: Ie9ed6a4bc4e7b3b306380dfdf7d363a875f5159a

--- package/bridge-utils/bridge-utils.mk
+++ package/bridge-utils/bridge-utils.mk
@@ -4,9 +4,7 @@
 #
 ################################################################################
 
-BRIDGE_UTILS_VERSION = 1.6
 BRIDGE_UTILS_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/net/bridge-utils
-BRIDGE_UTILS_SOURCE = bridge-utils-1.6.tar.xz
 BRIDGE_UTILS_AUTORECONF = YES
 BRIDGE_UTILS_CONF_OPTS = --with-linux-headers=$(LINUX_HEADERS_DIR)
 BRIDGE_UTILS_LICENSE = GPL-2.0+
