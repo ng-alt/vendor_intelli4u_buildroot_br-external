1bfac28fa57fcb95655dd01c51b9afac33162823
ccc <cadappl@gmail.com>
2018-11-14 23:56:36 +0800

    zebra: update package not build doc
    It's broken with makeinfo to build doc.
    
    Change-Id: I85316b5b9359d727222b0644924127307419d8ed

--- package/zebra/zebra.mk
+++ package/zebra/zebra.mk
@@ -16,4 +16,10 @@ ZEBRA_CONF_OPTS += $(if $(BR2_PACKAGE_ZEBRA_OSPF6D),--enable-ospf6d,--disable-os
 ZEBRA_CONF_OPTS += $(if $(BR2_PACKAGE_ZEBRA_BGP_ANNOUNCE),--enable-bgp-announce,--disable-bgp-announce)
 ZEBRA_CONF_OPTS += $(if $(BR2_PACKAGE_ZEBRA_TCP_ZEBRA),--enable-tcp-zebra,--disable-tcp-zebra)
 
+define ZEBRA_DISABLE_BUILD
+	$(SED) 's, doc,,' $(@D)/Makefile.am
+	$(SED) 's, doc,,' $(@D)/Makefile.in
+endef
+ZEBRA_PRE_CONFIGURE_HOOKS += ZEBRA_DISABLE_BUILD
+
 $(eval $(autotools-package))
