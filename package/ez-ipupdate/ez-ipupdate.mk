d525e735e0bf143e9767c4ffc8e445c8e0372523
ccc <cadappl@gmail.com>
2018-11-22 21:27:33 +0800

    ez-ipupdate: update makefile
    Change-Id: Id19ee51ec3526e95561e3e2ef21e68f99ffeb047

--- package/ez-ipupdate/ez-ipupdate.mk
+++ package/ez-ipupdate/ez-ipupdate.mk
@@ -5,15 +5,16 @@
 ################################################################################
 
 EZ_IPUPDATE_LICENSE = GPLv2
-EZ_IPUPDATE_DEPENDENCIES = router
-EZ_IPUPDATE_AUTORECONF = YES
+EZ_IPUPDATE_DEPENDENCIES = $(if $(BR2_PACKAGE_ROUTER),router)
 
 EZ_IPUPDATE_MAKE_OPTS = \
-    CFLAGS="-DASUS_DDNS -I$(ROOT)$(SRCBASE)/include -I$(ROUTER_DIR)/shared" \
+    CFLAGS="-DASUS_DDNS -I$(if $(SRC_BASE_DIR),$(SRC_BASE_DIR),$(BR2_TOPDIR)$(SRCBASE))/include -I$(ROUTER_DIR)/shared" \
 	LDFLAGS="-L$(TARGET_DIR)/usr/lib -lnvram -lshared -lgcc_s"
 
 define EZ_IPUPDATE_INSTALL_TARGET_CMDS
 	$(INSTALL) $(@D)/ez-ipupdate $(TARGET_DIR)/usr/sbin/ez-ipupdate
 endef
 
-$(eval $(autotools-package))
+EZ_IPUPDATE_PRE_BUILD_HOOKS += ROUTER_MAKEFILE_INCLUDE_SUPPRESS_SUBROUTINE
+
+$(eval $(make-package))
