a381abf2d621120f046ceef1e708c925a0673a3b
ccc <cadappl@gmail.com>
2018-11-22 21:29:08 +0800

    openpam: update makefile
    Change-Id: I2ff0fd5908f1a1757e63223fa46491d512b16f5e

--- package/openpam/openpam.mk
+++ package/openpam/openpam.mk
@@ -7,4 +7,9 @@
 OPENPAM_LICENSE = ​3-clause BSD license
 OPENPAM_AUTOGEN = YES
 
+define OPENPAM_UPDATE_AUTO_SCRIPT
+	$(SED) 's,autoconf,autoreconf -fi,' $(@D)/autogen.sh
+endef
+OPENPAM_PRE_CONFIGURE_HOOKS += OPENPAM_UPDATE_AUTO_SCRIPT
+
 $(eval $(autotools-package))
