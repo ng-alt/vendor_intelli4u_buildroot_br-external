ba622b8a3a6fb3aa93ad45aebf8faabd94534d39
ccc <cadappl@gmail.com>
2018-11-27 23:44:12 +0800

    rp-l2tp: update package
    Change-Id: Ieda077e35c28fff9b965f9afb07bdd82ba3292db

--- package/rp-l2tp/rp-l2tp.mk
+++ package/rp-l2tp/rp-l2tp.mk
@@ -7,10 +7,9 @@
 RP_L2TP_LICENSE = LGPLv2
 RP_L2TP_AUTORECONF = YES
 
-#RP_L2TP_CONF_OPTS = ac_cv_prog_AR=$(TARGET_AR)
-
 define RP_L2TP_INSTALL_TARGET_CMDS
-	$(INSTALL) $(@D)/l2tpd $(TARGET_DIR)/usr/sbin/l2tpd
+	$(INSTALL) -m 0755 $(@D)/l2tpd $(TARGET_DIR)/usr/sbin/l2tpd
+	$(INSTALL) -m 0644 $(@D)/handlers/cmd.so $(TARGET_DIR)/usr/lib
 endef
 
 $(eval $(autotools-package))
