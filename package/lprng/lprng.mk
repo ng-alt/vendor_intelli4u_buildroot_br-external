3cdd6dd0c2e8a972feacebf2a0db14ad8f7fb263
ccc <cadappl@gmail.com>
2018-11-29 07:58:24 +0800

    lprng: update package
    original lprng use Makefile to compile. buildroot uses autotools
    and the package has been patched with the changes in Makefile
    and here with the compilation flags.
    
    Change-Id: Ia1e567e94a48c4b9fe0dcc3a4c1ec3b959a9b810

--- package/lprng/lprng.mk
+++ package/lprng/lprng.mk
@@ -5,8 +5,11 @@
 ################################################################################
 
 LPRNG_LICENSE = GPLv2
+LPRNG_MAKE = $(MAKE1)
 
-LPRNG_CONF_ENV += CFLAGS="-I$(SRC_BASE_DIR)/include -I$(ROUTER_DIR)/shared" \
-	LDFLAGS="-L$(TARGET_DIR)/usr/lib -lnvram"
+LPRNG_CONF_OPTS = --disable-werror
+LPRNG_CONF_ENV += \
+	CFLAGS="-I$(SRC_BASE_DIR)/include -I$(ROUTER_DIR)/shared -DLPR_with_ASUS" \
+	LDFLAGS="-L$(TARGET_DIR)/usr/lib -lnvram -lshared -lgcc_s"
 
 $(eval $(autotools-package))
