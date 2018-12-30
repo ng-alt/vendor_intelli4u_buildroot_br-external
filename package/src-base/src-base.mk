f01f17102936aa72b5a5b206f126128dba7e4206
ccc <cadappl@gmail.com>
2018-12-30 17:12:51 +0800

    src-base: support to customize original target config
    Change-Id: I0166442ba9a7641ef8e9664e75324623765f031b

--- package/src-base/src-base.mk
+++ package/src-base/src-base.mk
@@ -10,10 +10,20 @@ SRC_BASE_VERSION_PATTERN = "\#\s*define\s+AMBIT_SOFTWARE_VERSION\s+\"([^\"]+)\""
 
 SRC_BASE_MAKE = $(MAKE1)
 
+SRC_BASE_OVERRIDED_TARGET = $(call qstrip,$(BR2_PACKAGE_SRC_BASE_OVERRIDED_TARGET))
+# build the referred .config with original asus solution.
+# target.mak contains the initial configuration for router variants and the overrided
+# includes the patches for the file. Append profile.merlin.alt.mak to profile.mak to
+# adjust some configurations
 define SRC_BASE_BUILD_CMDS
 	for script in platform.mak profile.mak target.mak version.conf ; do \
 		rsync -au $(SRC_BASE_REF_DIR)/$$script $(@D)/; \
 	done
+
+	if [ -n "$(SRC_BASE_OVERRIDED_TARGET)" ] ; then \
+		echo "" >> $(@D)/target.mak; \
+		cat $(SRC_BASE_OVERRIDED_TARGET) >> $(@D)/target.mak; \
+	fi
 
 	rsync -au $(SRC_BASE_REF_DIR)/Makefile $(@D)/Makefile.asus && \
 		$(SED) '/$$(MAKE) bin/d' -e 's,router/,./,g' $(@D)/Makefile.asus && \
