69f2d215539b2eb6ab63bd04ff8fb2ed25a2c2bf
ccc <cadappl@gmail.com>
2019-01-07 23:49:57 +0800

    r6300v2: remove the dependencies of original source
    Change-Id: Id0bae0fe8a753ebd4c0b7ce55bd081e651c6dd66

--- scripts/post-image.sh
+++ scripts/post-image.sh
@@ -5,16 +5,18 @@ TMP_DIR=tmp.$$
 
 MERL_CONF=$BR2_TOPDIR/vendor/asus/base/version.conf
 
-BASE_VERSION=$(ls -d $BR2_OUTDIR/build/src-base-V* | grep -o -P 'base-.*' | sed -n 's,base-V\(.*\),\1,p')
+#BASE_VERSION=$(ls -d $BR2_OUTDIR/build/router-* | grep -o -P 'router-.*' | sed -n 's,router-\(.*\),\1,p')
 if [ -e "$MERL_CONF" ] ; then
   MERLIN_VERSION=$(grep -o -P 'SERIALNO=.*' $MERL_CONF | sed -n 's,SERIALNO=\(.*\),\1,p')
 fi
 
-if [ -z "$MERLIN_VERSION" ] ; then
-  VERSION="$BASE_VERSION"
-else
-  VERSION="${BASE_VERSION}_${MERLIN_VERSION}"
-fi
+#if [ -z "$MERLIN_VERSION" ] ; then
+  VERSION="$MERLIN_VERSION"
+#else
+#  VERSION="${BASE_VERSION}_${MERLIN_VERSION}"
+#fi
+
+variant=$(echo "$PROFILE" | tr A-Z a-z)
 
 cd $BR2_OUTDIR/$IMG_DIR && (
 
@@ -26,9 +28,9 @@ cd $BR2_OUTDIR/$IMG_DIR && (
     cd $TMP_DIR && \
     touch rootfs && \
     packet -k ../linux.trx -f rootfs \
-      -b $BR2_TOPDIR/vendor/netgear/router/arm-uclibc/compatible_$(echo "$PROFILE" | tr A-Z a-z).txt \
+      -b $BR2_TOPDIR/vendor/intelli4u/buildroot/board/$variant/compatible_$variant.txt \
       -ok kernel_image -oall ../${PROFILE}-${VERSION}_$(date +%m%d-%H%M) -or rootfs_image \
-      -i $BR2_TOPDIR/$ACOS/include/ambitCfg.h && \
+      -i $BR2_TOPDIR/vendor/intelli4u/buildroot/board/$variant/ambitCfg.h && \
     rm -rf rootfs
   )
 
