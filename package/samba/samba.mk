edb673f3ff589b2418ec1c8c957e7ecbe4b7f542
ccc <cadappl@gmail.com>
2019-01-13 23:52:12 +0800

    samba: update install commands
    Change-Id: Idd50d611312459b4292b0aaa3fab45c4558de46f

--- package/samba/samba.mk
+++ package/samba/samba.mk
@@ -18,4 +18,12 @@ SAMBA_PRE_BUILD_HOOKS += ROUTER_MAKEFILE_INCLUDE_SUPPRESS_SUBROUTINE
 SAMBA_MAKE_OPTS = $(ROUTER_PREPARED_MAKE_OPTS)
 SAMBA_INSTALL_TARGET_OPTS = $(SAMBA_MAKE_OPTS)
 
+define SAMBA_INSTALL_TARGET_CMDS
+	install -D $(@D)/source3/bin/samba_multicall $(TARGET_DIR)/usr/bin/samba_multicall
+	ln -sf ../bin/samba_multicall $(TARGET_DIR)/usr/sbin/smbd
+	ln -sf ../bin/samba_multicall $(TARGET_DIR)/usr/sbin/nmbd
+	ln -sf samba_multicall $(TARGET_DIR)/usr/bin/smbpasswd
+	$(TARGET_STRIP) -s $(TARGET_DIR)/usr/bin/samba_multicall
+endef
+
 $(eval $(make-package))
