a76f3892ac385dbb2b290ac85b6c76584bd97091
ccc <cadappl@gmail.com>
2019-02-17 17:58:35 +0800

    post-image: build trx for asuswrt-merlin
    Change-Id: I1b982e2aaba664a5e361334a16366cbb58c63879

--- scripts/post-image.sh
+++ scripts/post-image.sh
@@ -5,9 +5,13 @@ TMP_DIR=tmp.$$
 
 MERL_CONF=$BR2_TOPDIR/vendor/asus/base/version.conf
 
+if [ -e $MERL_CONF ] ; then
+  source $MERL_CONF
+fi
+
 #BASE_VERSION=$(ls -d $BR2_OUTDIR/build/router-* | grep -o -P 'router-.*' | sed -n 's,router-\(.*\),\1,p')
 if [ -e "$MERL_CONF" ] ; then
-  MERLIN_VERSION=$(grep -o -P 'SERIALNO=.*' $MERL_CONF | sed -n 's,SERIALNO=\(.*\),\1,p')
+  MERLIN_VERSION=$SERIALNO
 fi
 
 #if [ -z "$MERLIN_VERSION" ] ; then
@@ -19,20 +23,27 @@ fi
 variant=$(echo "$PROFILE" | tr A-Z a-z)
 
 cd $BR2_OUTDIR/$IMG_DIR && (
+  PREFIX=${PROFILE}-${VERSION}_$(date +%m%d-%H%M)
 
-  #-- build trx file
-  trx -o linux.trx vmlinuz rootfs.squashfs
+  mkdir -p $TMP_DIR && (
+    cd $TMP_DIR
+    touch rootfs
 
-  #-- build chk file
-  mkdir -p $TMP_DIR && ( \
-    cd $TMP_DIR && \
-    touch rootfs && \
-    packet -k ../linux.trx -f rootfs \
+    trx -o linux.trx ../vmlinuz ../rootfs.squashfs
+
+    #-- build (asus) trx file
+    trx_asus -i linux.trx -r $BUILD_NAME,$KERNEL_VER.$FS_VER,$SERIALNO,$EXTENDNO,$PREFIX.trx && \
+      mv $PREFIX.trx ..
+
+    #-- build chk file
+    packet -k linux.trx -f rootfs \
       -b $BR2_TOPDIR/vendor/intelli4u/buildroot/board/$variant/compatible_$variant.txt \
-      -ok kernel_image -oall ../${PROFILE}-${VERSION}_$(date +%m%d-%H%M) -or rootfs_image \
-      -i $BR2_TOPDIR/vendor/intelli4u/buildroot/board/$variant/ambitCfg.h && \
+      -ok kernel_image -oall ../$PREFIX -or rootfs_image \
+      -i $BR2_TOPDIR/vendor/intelli4u/buildroot/board/$variant/ambitCfg.h
+
     rm -rf rootfs
   )
 
   rm -rf $TMP_DIR
 )
+
