1e0682f0a85f5acf9071e8b692960699ee65674d
ccc <cadappl@gmail.com>
2019-05-10 23:28:57 +0800

    usbmodem: add KDIR for LINUX_DIR to build kernel module
    Change-Id: I4943e1961dac0b7843ac15e8402dcf353886ef28

--- package/usbmodem/usbmodem.mk
+++ package/usbmodem/usbmodem.mk
@@ -5,11 +5,11 @@
 ################################################################################
 
 define USBMODEM_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILE=$(TARGET_CC) -C $(@D)/driver
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILE=$(TARGET_CC) KDIR=$(LINUX_DIR) -C $(@D)/driver
 endef
 
 define USBMODEM_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILE=$(TARGET_CC) -C $(@D)/driver install TARGETDIR=$(TARGET_DIR)
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILE=$(TARGET_CC) KDIR=$(LINUX_DIR) -C $(@D)/driver install TARGETDIR=$(TARGET_DIR)
 endef
 
 $(eval $(generic-package))
