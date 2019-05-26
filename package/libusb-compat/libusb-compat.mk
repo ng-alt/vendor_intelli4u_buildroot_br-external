2c330665f30174881c0f60aad12175a6c2c37628
ccc <cadappl@gmail.com>
2019-05-26 21:50:15 +0800

    libusb-compat: enable autogen for pre-configure
    Change-Id: I9960918c89ac05dfbf14c166fa593adb23d2004d

--- package/libusb-compat/libusb-compat.mk
+++ package/libusb-compat/libusb-compat.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBUSB_COMPAT_SITE = http://downloads.sourceforge.net/project/libusb/libusb-compat-$(LIBUSB_COMPAT_VERSION_MAJOR)/libusb-compat-$(LIBUSB_COMPAT_VERSION)
 LIBUSB_COMPAT_DEPENDENCIES = host-pkgconf libusb
+LIBUSB_COMPAT_AUTOGEN = YES
 LIBUSB_COMPAT_INSTALL_STAGING = YES
 LIBUSB_COMPAT_CONFIG_SCRIPTS = libusb-config
 LIBUSB_COMPAT_LICENSE = LGPL-2.1+
