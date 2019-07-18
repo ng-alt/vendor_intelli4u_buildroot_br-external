d640bab404c5dd4531618ca91b4a4db47ca26dcc
ccc <cadappl@gmail.com>
2019-07-18 23:18:48 +0800

    external.sh: set post_lunch to initialize path for variants
    Change-Id: I8a4cfa31bb720a2d741499953b988a7d3baa268d

--- external.sh
+++ external.sh
@@ -1,7 +1,12 @@
 
-insert_path $BR2_TOPDIR/prebuilts/router/tools/$BR2_PRODUCT
+insert_path $BR2_TOPDIR/prebuilts/router/tools
 # Add trx_asus
 insert_path $BR2_TOPDIR/vendor/asus/base/ctools/prebuild
+
+unset -f post_lunch
+function post_lunch() {
+  insert_path $BR2_TOPDIR/prebuilts/router/tools/$BR2_PRODUCT
+}
 
 add_lunch_combo r6300v2
 add_lunch_combo r6400
