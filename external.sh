1f0bf41d99dedeab1c9561185f6611f79eb59c9f
ccc <cadappl@gmail.com>
2019-07-20 08:10:07 +0800

    external.sh: add host bin directory in post_lunch
    Depends-On: Ic10e03121d417f719205f593db407207d1848a37
    Change-Id: I0a207118cff2dcd941b1c4e03d67aec88cdbac37

--- external.sh
+++ external.sh
@@ -6,6 +6,7 @@ insert_path $BR2_TOPDIR/vendor/asus/base/ctools/prebuild
 unset -f post_lunch
 function post_lunch() {
   insert_path $BR2_TOPDIR/prebuilts/router/tools/$BR2_PRODUCT
+  insert_path_f $BR2_OUTDIR/host/bin
 }
 
 add_lunch_combo r6300v2
